e8acba64ae6b331af92312dbb016351a4e95efff
michaelmondong <95898805+michaelmondong@users.noreply.github.com>
2024-02-06 08:31:37 +0700

    Create logouttest.py

---
+++ logouttest.py
@@ -0,0 +1,46 @@
+import unittest, os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class LogoutTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        option = webdriver.EdgeOptions()
+        cls.browser = webdriver.Edge(options=option)
+        try:
+            cls.url = os.environ['URL']
+        except:
+            cls.url = "http://localhost"
+
+    def test(self):
+        self.login_correct()
+        self.index_page()
+        self.logout()
+
+    def login_correct(self):
+        login_url = self.url +'/login.php'
+        self.browser.get(login_url)
+
+        self.browser.find_element(By.ID, 'inputUsername').send_keys('admin')
+        self.browser.find_element(By.ID, 'inputPassword').send_keys('nimda666!')
+        self.browser.find_element(By.TAG_NAME, 'button').click()
+
+    def index_page(self):
+        expected_result = "admin"
+        actual_result = self.browser.find_element(By.XPATH, "//h2[contains(text(),'Halo,')]").text.split(', ')[1]
+        self.assertIn(expected_result, actual_result)
+
+    def logout(self):
+        self.browser.find_element(By.XPATH, "//a[contains(text(),'Sign out')]").click()
+
+        login_page_title = "Login"
+        actual_title = self.browser.title
+        self.assertEqual(login_page_title, actual_title)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2, warnings='ignore')
