a0322fafc37acf25683e1e1a7f29bdb564854a57
michaelmondong <95898805+michaelmondong@users.noreply.github.com>
2024-02-06 08:47:21 +0700

    Update profiletest.py

--- profiletest.py
+++ profiletest.py
@@ -6,8 +6,8 @@ class ProfileTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        option = webdriver.EdgeOptions()
-        cls.browser = webdriver.Edge(options=option)
+        option = webdriver.FirefoxOptions()
+        cls.browser = webdriver.Firefox(options=option)
         try:
             cls.url = os.environ['URL']
         except:
