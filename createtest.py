2b314940f804c3e1bd473c3c199803c50b74ebef
michaelmondong <95898805+michaelmondong@users.noreply.github.com>
2024-02-06 10:09:55 +0700

    Update createtest.py

--- createtest.py
+++ createtest.py
@@ -34,46 +34,7 @@ class CreateContact(unittest.TestCase):
         self.browser.find_element(By.ID, 'inputPassword').send_keys('nimda666!')
         self.browser.find_element(By.TAG_NAME, 'button').click()
 
-    def create_contact(self):
-        create_url = self.url + '/create.php'
-        self.browser.get(create_url)
-
-        self.browser.find_element(By.ID, 'name').send_keys('michael')
-        self.browser.find_element(By.ID, 'email').send_keys('michael@example.com')
-        self.browser.find_element(By.ID, 'phone').send_keys('081234567890')
-        self.browser.find_element(By.ID, 'title').send_keys('Cadet')
-
-        self.browser.find_element(By.CSS_SELECTOR, 'input[type="submit"]').click()
-
-        index_page_title = "Dashboard"
-        actual_title = self.browser.title
-        self.assertEqual(index_page_title, actual_title)
-
-    def search_contact(self):
-        search_query = self.name_query
-        self.browser.find_element(By.ID, 'employee_filter').find_element(By.TAG_NAME, 'input').send_keys(search_query)
-        self.browser.find_element(By.ID, 'employee_filter').find_element(By.TAG_NAME, 'input').send_keys(Keys.ENTER)
-
-        searched_contact_name = self.name_query
-        searched_contact_exists = self.browser.find_elements(By.XPATH, f"//td[contains(text(), '{searched_contact_name}')]")
-        self.assertTrue(searched_contact_exists)
-
-    def delete_contact(self):
-        actions_section = self.browser.find_element(By.XPATH, "//tr[@role='row'][1]//td[contains(@class, 'actions')]")
-        delete_button = actions_section.find_element(By.XPATH, ".//a[contains(@class, 'btn-danger')]")
-
-        delete_button.click()
-
-        self.browser.switch_to.alert.accept()
-        time.sleep(3)
-
-        search_query = self.name_query
-        self.browser.find_element(By.ID, 'employee_filter').find_element(By.TAG_NAME, 'input').send_keys(search_query)
-        self.browser.find_element(By.ID, 'employee_filter').find_element(By.TAG_NAME, 'input').send_keys(Keys.ENTER)
-
-        searched_contact_name = self.name_query
-        searched_contact_exists = self.browser.find_elements(By.XPATH, f"//td[contains(text(), '{searched_contact_name}')]")
-        self.assertFalse(searched_contact_exists)
+    
 
     @classmethod
     def tearDownClass(cls):
